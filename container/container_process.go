98f5bd9dbde73b462eb191830a1e248c5ea0e7ca
pjm <2556323541@qq.com>
2024-02-17 12:28:54 +0800

    optimize: 使用匿名管道传输参数

--- container/container_process.go
+++ container/container_process.go
@@ -1,6 +1,7 @@
 package container
 
 import (
+	"github.com/sirupsen/logrus"
 	"os"
 	"os/exec"
 	"syscall"
@@ -14,9 +15,18 @@ import (
 3.下面的clone参数就是去fork出来一个新进程，并且使用了namespace隔离新创建的进程和外部环境。
 4.如果用户指定了-it参数，就需要把当前进程的输入输出导入到标准输入输出上
 */
-func NewParentProcess(tty bool, command string) *exec.Cmd {
-	args := []string{"init", command}
+func NewParentProcess(tty bool) (*exec.Cmd, *os.File, error) {
+	// 创建匿名管道用于传递参数，将readPipe作为子进程的ExtraFiles，子进程从readPipe中读取参数
+	// 父进程中则通过writePipe将参数写入管道
+	readPipe, writePipe, err := os.Pipe()
+	if err != nil {
+		logrus.Errorf("new pipe fail, %v", err)
+		return nil, nil, err
+	}
+
+	args := []string{"init"}
 	cmd := exec.Command("/proc/self/exe", args...)
+	cmd.ExtraFiles = []*os.File{readPipe}
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
 	}
@@ -26,5 +36,6 @@ func NewParentProcess(tty bool, command string) *exec.Cmd {
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 	}
-	return cmd
+
+	return cmd, writePipe, nil
 }
